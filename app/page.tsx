89a7ac15d36b0eda13c9f0d586481b86452dc489
Jordan Higuera <jordanwhiguera@gmail.com>
2023-12-28 19:02:46 -0500

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import HeroTwo from "./components/HeroTwo";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <HeroTwo />
+      <Footer />
     </>
   );
 }
