1adebae3230fa4531c3c7a59736df83da834ac1d
Jordan Higuera <jordanwhiguera@gmail.com>
2024-01-12 20:53:19 -0500

    UPDATED form spree api logic

--- pages/api/submit-form.ts
+++ pages/api/submit-form.ts
@@ -1,37 +1,33 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    try {
-      // Use the environment variable for the Formspree URL
-      const formEndpoint = process.env.FORMSPREE_ENDPOINT;
-
-      // Check if the Formspree endpoint URL is set
-      if (!formEndpoint) {
-        throw new Error("Formspree endpoint URL is not set in environment variables");
-      }
-
-      const response = await fetch(formEndpoint, {
-        method: "POST",
-        body: JSON.stringify(req.body),
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error(`Error from Formspree: ${response.statusText}`);
-      }
-
-      res.status(200).json({ message: "Form submitted successfully" });
-    } catch (error) {
-      console.error("Error in API Route:", error);
-      res.status(500).json({ error: "Server error" });
-    }
-  } else {
+  if (req.method !== "POST") {
     res.setHeader("Allow", ["POST"]);
-    res.status(405).end("Method Not Allowed");
+    return res.status(405).end("Method Not Allowed");
+  }
+
+  const formEndpoint = process.env.FORMSPREE_ENDPOINT;
+
+  if (!formEndpoint) {
+    return res.status(500).json({ error: "Formspree endpoint URL is not configured" });
+  }
+
+  try {
+    const response = await fetch(formEndpoint, {
+      method: "POST",
+      body: JSON.stringify(req.body),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      return res.status(500).json({ error: `Error from Formspree: ${response.statusText}` });
+    }
+
+    return res.status(200).json({ message: "Form submitted successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Server error" });
   }
 }
