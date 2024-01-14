9505802419d6193de4e62d8b2830235f5eac1d8b
Jordan Higuera <jordanwhiguera@gmail.com>
2024-01-14 17:50:14 -0500

    added portfolio and about page

---
+++ app/(routes)/about/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Overlay from "@/app/components/Overlay";
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <>
+      <Overlay
+        imageSrc="/images/man-phone.webp"
+        imageAlt="man looking at phone"
+        text="About"
+      />
+    </>
+  );
+};
+
+export default AboutPage;
---
+++ app/(routes)/portfolio/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Overlay from "@/app/components/Overlay";
+import React from "react";
+
+const PortfolioPage = () => {
+  return (
+    <>
+      <Overlay
+        imageSrc="/images/man-phone.webp"
+        imageAlt="man looking at phone"
+        text="Portfolio"
+      />
+    </>
+  );
+};
+export default PortfolioPage;
