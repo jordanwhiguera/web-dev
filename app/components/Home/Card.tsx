fcc9ed363eb101753c4db3c0b753e2e6c8bed8b8
Jordan Higuera <jordanwhiguera@gmail.com>
2024-01-17 14:39:22 -0500

    changed card text

--- app/components/Home/Card.tsx
+++ app/components/Home/Card.tsx
@@ -17,7 +17,7 @@ const cardItems: CardItem[] = [
     id: 1,
     title: "Personalized Web Development Experience",
     description:
-      "At Jordy Web Development & Design, I prioritize your unique needs. Serving Port St. Lucie and Treasure Coast regions, I offer personalized web development and design services tailored to your project's size and scope. With a focus on client-centric solutions, I ensure your voice is integral in every step, delivering web solutions that truly resonate with our community.",
+      "At Jordy Web Development & Design, your unique needs are my focus. I offer custom web development and design services in Port St. Lucie and the Treasure Coast, ensuring a personalized approach for every project.",
 
     imageUrl: "/images/pc-people.webp", // Replace with your image path
   },
@@ -25,7 +25,7 @@ const cardItems: CardItem[] = [
     id: 2,
     title: "Hand-Coded, High-Performance Websites",
     description:
-      "Specializing in custom-coded websites, I ensure superior performance, speed, and SEO. Every website is handcrafted to deliver faster load times and improved search rankings, setting your digital presence apart. Experience the impact of a visually striking, functionally robust website, fine-tuned for digital excellence.",
+      "I specialize in hand-coding websites to ensure top-notch performance and enhanced SEO. Each site is crafted to provide faster load times and a distinct digital presence.",
 
     imageUrl: "/images/laptop.webp", // Replace with your image path
   },
@@ -33,7 +33,7 @@ const cardItems: CardItem[] = [
     id: 3,
     title: "Optimized Responsive Design for All Devices",
     description:
-      "Understanding the importance of a multi-device world, I create websites that are stunning and functional on every screen. Prioritizing responsive design, your site will provide a seamless user experience from smartphones to desktops, enhancing customer engagement and showcasing your business at its best across all platforms.",
+      "In today's multi-device world, I create websites that look great and function seamlessly on all screens. Responsive design is a priority, ensuring a superb user experience across smartphones, tablets, and desktops.",
 
     imageUrl: "/images/man-phone.webp", // Replace with your image path
   },
